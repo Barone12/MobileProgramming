e11c5ab96eca7b72dd9846d2ea795f4c39995607
Jeong jaeuk <100332233+Barone12@users.noreply.github.com>
2025-05-22 19:02:50 +0900

    create Week12

---
+++ Week12/src/app/index.js
@@ -0,0 +1,20 @@
+﻿import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+    return (
+        <View style={}>
+            <Text>여기는 본문 내용이 들어갈 장소입니다.</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
---
+++ Week12/src/app/server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = 4000;
+const DATA_FILE = path.join(__dirname, 'data.json');
+const allowedOrigins = [
+    'http://localhost:8081', 'http://127.0.0.1:8081',
+    'app://com.example.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (origin && allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("허용되지 않은 출처"));
+        }
+    }
+}));
